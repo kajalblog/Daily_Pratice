9e2a3db2349a90c0e2ae3e5c3cc23691afc9e1d8
kajal <kajalparchande@gmail.com>
2022-02-17 21:57:18 +0530

    'unionFile'

---
+++ union.ts
@@ -0,0 +1,18 @@
+// Union Type
+let data:string|number|boolean="Kajal";
+// data=30;
+data=true;
+console.log(data);
+
+function add1(a1:number|string,b1:number|string)
+{
+        if(typeof a1==="number"&& typeof b1==='number')
+        {
+            return a1+b1;
+        }
+        else{
+            return a1.toString()+b1.toString();
+        }
+}
+console.log(add1(15,16));
+console.log(add1("kajal","Parchande"));
