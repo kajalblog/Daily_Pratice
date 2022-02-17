1dc2fd3fbd83185d8b7cead0e2cec0ea787fdd3d
kajal <kajalparchande@gmail.com>
2022-02-17 22:06:11 +0530

    'anyFile'

---
+++ any.ts
@@ -0,0 +1,8 @@
+// any can store any type of variable
+
+let arr4:any=10;
+arr4="kajal";
+console.log(arr4);
+
+let arr5:any[]=[10];   /* if it took array then we have to provide array */
+console.log(arr5);
