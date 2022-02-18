4c6501f8bb0aee2e068ede2e11b87a90afedfc83
kajal <kajalparchande@gmail.com>
2022-02-18 20:35:28 +0530

    'pvtFile'

---
+++ private.ts
@@ -0,0 +1,19 @@
+class User
+{
+  private  name:"";
+
+    setUser(name)
+    {
+        this.name=name;
+    }
+    getUser()
+    {
+        console.log(this.name);
+    }
+}
+var u1=new User;
+u1.setUser("kajal");
+// u1.name="Tejal";
+u1.getUser();
+
+// ul.getUser();
